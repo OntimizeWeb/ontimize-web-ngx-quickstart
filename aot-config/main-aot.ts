93a791ed61082b6ce386ab337f9a4fbbb0f2a91f
gonzalo.martinez <gonzalo.martinez@imatia.com>
2017-11-15 16:08:12 +0100

    Updating config files

--- aot-config/main-aot.ts
+++ aot-config/main-aot.ts
@@ -3,16 +3,12 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowser } from '@angular/platform-browser';
 import { AppModuleNgFactory } from './app/app.module.ngfactory';
 
-// import { COMPILER_PROVIDERS } from '@angular/compiler';
-
-
 import { ontimizePostBootstrap } from 'ontimize-web-ngx';
 
 // if (environment.production) {
 enableProdMode();
 // }
 
-/*COMPILER_PROVIDERS*/
 const promise = platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
 promise.then(ontimizePostBootstrap).catch(err => {
   console.error(err.message);
--- aot-config/webpack-aot.config.js
+++ aot-config/webpack-aot.config.js
@@ -13,7 +13,7 @@ const config = {
     main: helpers.root('tmp-src/main-aot.ts'),
     polyfills: helpers.root('tmp-src/polyfills.ts'),
     vendor: helpers.root('tmp-src/vendor-aot.ts'),
-    scripts: ["tmp-src/../node_modules/jquery/dist/jquery.js","tmp-src/../node_modules/colresizable/colResizable-1.6.min.js","tmp-src/../node_modules/pdfmake/build/pdfmake.js","tmp-src/../node_modules/pdfmake/build/vfs_fonts.js","tmp-src/assets/js/domchange.js","tmp-src/assets/js/keyboard.js"]
+    scripts: APP_SCRIPTS
   },
 
   // devtool: 'source-map',
