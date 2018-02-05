18f5b2a31a4d6d44b35d960b7a898b5d4e2f1956
gonzalo.martinez <gonzalo.martinez@imatia.com>
2018-02-05 14:46:08 +0100

    Update production environment data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // apiEndpoint: 'https://try.ontimize.com/QSAllComponents-jee/services/rest',
-  apiEndpoint: 'http://37.139.121.180:9090/qsallcomponents-jee/services/rest',
+  apiEndpoint: 'https://try.imatia.com/ontimizeweb/services/qsallcomponents-jee/services/rest',
   production: true
 };
