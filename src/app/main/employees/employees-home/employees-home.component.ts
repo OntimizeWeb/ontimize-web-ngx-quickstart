3526d665d46d73b10a8326f4f15df329d26e6a0f
Manuel cid <manuel.cid@imatia.com>
2018-06-06 13:42:32 +0200

    Include 'o-filter-builder' component on employees home page

--- src/app/main/employees/employees-home/employees-home.component.ts
+++ src/app/main/employees/employees-home/employees-home.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, } from '@angular/core';
+
+import { FilterExpressionUtils, IExpression } from 'ontimize-web-ngx';
 
 @Component({
-  selector: 'app-employees-home',
+  selector: 'employees-home',
   templateUrl: './employees-home.component.html',
   styleUrls: ['./employees-home.component.scss']
 })
-export class EmployeesHomeComponent implements OnInit {
+export class EmployeesHomeComponent {
 
-  constructor() { }
+  createFilter(values: Array<{ attr, value }>): IExpression {
+    // Prepare simple expressions from the filter components values
+    let filters: Array<IExpression> = [];
+    values.forEach(fil => {
+      if (fil.value) {
+        if (fil.attr === 'EMPLOYEENAME' || fil.attr === 'EMPLOYEESURNAME') {
+          filters.push(FilterExpressionUtils.buildExpressionLike(fil.attr, fil.value));
+        }
+        if (fil.attr === 'EMPLOYEETYPEID') {
+          filters.push(FilterExpressionUtils.buildExpressionEquals(fil.attr, fil.value));
+        }
+      }
+    });
 
-  ngOnInit() {
+    // Build complex expression
+    if (filters.length > 0) {
+      return filters.reduce((exp1, exp2) => FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND));
+    } else {
+      return null;
+    }
   }
 
 }
