e8167a082cff1bc6de6927dc991be91834fe79bc
manu-cr <manuelcidrodriguez@gmail.com>
2018-08-07 16:12:57 +0200

    Remove unnecessary code

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,15 +1,8 @@
-import { ViewEncapsulation, Injector, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./home.component.scss']
 })
-
-export class HomeComponent {
-
-  constructor(private injector: Injector) {
-
-  }
-}
+export class HomeComponent { }
