472d629baf6426c7603a1a16323a303f62ed4962
Patricia Martinez <patricia.martinez@imatia.com>
2019-01-07 14:28:16 +0100

    Added OMapModule in branches-detail

--- src/app/main/branches/branches.module.ts
+++ src/app/main/branches/branches.module.ts
@@ -4,13 +4,16 @@ import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { SharedModule } from '../../shared/shared.module';
 import { BranchesRoutingModule, BRANCHES_MODULE_DECLARATIONS } from './branches-routing.module';
 import { OChartModule } from 'ontimize-web-ngx-charts';
+import { OMapModule } from 'ontimize-web-ngx-map';
+
 
 @NgModule({
   imports: [
     SharedModule,
     OntimizeWebModule,
     OChartModule,
-    BranchesRoutingModule
+    BranchesRoutingModule,
+    OMapModule
   ],
   declarations: BRANCHES_MODULE_DECLARATIONS,
   exports: BRANCHES_MODULE_DECLARATIONS
