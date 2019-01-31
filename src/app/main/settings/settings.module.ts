78add903a114d286ee8a0f9bc0d74239086a5ff1
Manuel Cid <manuelcidrodriguez@gmail.com>
2019-01-31 18:17:14 +0100

    Settings page

--- src/app/main/settings/settings.module.ts
+++ src/app/main/settings/settings.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
+
 import { SharedModule } from '../../shared/shared.module';
-import { SettingsComponent } from './settings.component';
 import { SettingsRoutingModule } from './settings-routing.module';
+import { SettingsComponent } from './settings.component';
 
 @NgModule({
   imports: [
