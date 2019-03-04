d185efe3e802c2d8070b378c17d7644a46d4fdb0
manuel-cid <manuel.cid@imatia.com>
2019-03-04 12:07:53 +0100

    Update movements detail

--- src/app/main/accounts/detail/transaction/account-transaction.component.ts
+++ src/app/main/accounts/detail/transaction/account-transaction.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'account-transaction',
   styleUrls: ['./account-transaction.component.scss'],
-  templateUrl: './account-transaction.component.html'
+  templateUrl: './account-transaction.component.html',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    '[class.account-transaction]': 'true'
+  }
 })
 export class AccountTransactionsDetailComponent { }
