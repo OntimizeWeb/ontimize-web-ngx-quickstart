c337c14243bfaa0af64625b1c7a4480d54b3f1af
Patricia Martinez <patricia.martinez@imatia.com>
2019-06-21 09:46:39 +0200

    Updated properties environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   apiEndpoint: 'https://try.imatia.com/ontimizeweb/services/qsallcomponents-jee/services/rest',
   production: true,
   versions: {
-    core: '4.0.5',
-    charts: '4.0.1',
+    core: '4.0.6',
+    charts: '4.0.3',
     filemanager: '4.0.0',
     map: '4.0.0',
     theming: '4.0.2',
