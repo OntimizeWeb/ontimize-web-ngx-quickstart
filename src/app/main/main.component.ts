12988b38d4c4da48b2525e75f178ba48030b92ef
Patricia Martinez <patricia.martinez@imatia.com>
2020-05-29 10:50:45 +0200

    Fixed not loading general css

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MainComponent  {
 
