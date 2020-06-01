654a2dc5ce05043ccaf0525ce706bd2ada706cac
Patricia Martinez <patricia.martinez@imatia.com>
2020-06-01 12:01:48 +0200

    Fixed showing default or stored theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ThemeService } from './shared/theme.service';
+import { Util } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'o-app',
@@ -6,4 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(
+    private _themeService: ThemeService
+  ) {
+    const theme = this._themeService.getStoredTheme();
+    if (!Util.isDefined(theme)) {
+      this._themeService.currentTheme = this._themeService.getDefaultTheme();
+      this._themeService.storeTheme(this._themeService.currentTheme);
+    } else {
+      this._themeService.installTheme(theme);
+    }
+  }
 }
