6b72413f38fef6e9a96650c2a6705e398731e349
Patricia Martinez <patricia.martinez@imatia.com>
2020-11-25 11:06:16 +0100

    Updated ontimize-web-ngx@8.1.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   apiEndpoint: 'https://try.imatia.com/ontimizeweb/services/qsallcomponents-jee/services/rest',
   production: true,
   versions: {
-    core: '8.1.0',
+    core: '8.1.1',
     charts: '8.0.0',
     filemanager: '8.1.0',
     map: '8.0.1',
