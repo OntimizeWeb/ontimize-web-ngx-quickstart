baf9295ab531a26ca55abadf6197cd0524f664d1
Paco Gomez Imatia <francisco.gomez@imatia.com>
2021-12-03 09:30:48 +0100

    Update about page with new versions

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,9 +7,9 @@ export const environment = {
   apiEndpoint: 'https://try.imatia.com/ontimizeweb/services/qsallcomponents-jee/services/rest',
   production: false,
   versions: {
-    core: '8.5.1',
+    core: '8.5.5',
     charts: '8.0.0',
-    filemanager: '8.5.1',
+    filemanager: '8.5.4',
     map: '8.0.1',
     theming: '8.1.0'
   }
