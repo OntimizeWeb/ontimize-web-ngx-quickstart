9189b6dd638952925d32e506240a908aa8096495
Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
2022-03-22 12:05:57 +0100

    Updated ontimize-web-ngx versions (#75)
    * Updated ontimize-web-ngx versions
    
    * Added full-screen-button="true" in o-image in customer detail

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,10 +7,10 @@ export const environment = {
   apiEndpoint: 'https://try.imatia.com/ontimizeweb/services/qsallcomponents-jee/services/rest',
   production: false,
   versions: {
-    core: '8.5.7',
-    charts: '8.0.0',
+    core: '8.5.10',
+    charts: '8.0.1',
     filemanager: '8.5.4',
     map: '8.0.1',
-    theming: '8.1.0'
+    theming: '8.2.1'
   }
 };
