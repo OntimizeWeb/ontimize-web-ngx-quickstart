5e454b4d17ac8502917e93facf02f7ea804600ea
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2022-07-05 08:05:09 +0200

    Fixed that only the first report of the branch detail was generated

--- src/app/main/branches/home/branches-home.component.ts
+++ src/app/main/branches/home/branches-home.component.ts
@@ -1,5 +1,4 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { OTableButtonComponent, OTableComponent } from 'ontimize-web-ngx';
+import { Component } from '@angular/core';
 import { OReportStoreService } from 'ontimize-web-ngx-report';
 
 @Component({
@@ -7,34 +6,20 @@ import { OReportStoreService } from 'ontimize-web-ngx-report';
   templateUrl: './branches-home.component.html',
   styleUrls: ['./branches-home.component.scss']
 })
-export class BranchesHomeComponent implements AfterViewInit {
-
-  @ViewChild('button', { static: false })
-  protected button: OTableButtonComponent;
-  @ViewChild('detailButton', { static: false })
-  protected detailButton: OTableButtonComponent;
-  @ViewChild('branchEmployeesTable', { static: false })
-  protected branchEmployeesTable: OTableComponent;
-
-  public id: any;
-  public params: object;
-
-  ngAfterViewInit() {
-    this.button.onClick.subscribe(event => {
-      this.reportStoreService.openFillReport("96c0aa72-2582-4aa4-a93a-afe34acb77c6", {}, {});
-    });
-  }
+export class BranchesHomeComponent {
 
   constructor(private reportStoreService: OReportStoreService) { }
 
-  onDataLoaded(e: object) {
-    this.id = this.branchEmployeesTable.getParentKeysValues()['OFFICEID'];
-    this.params = {
-      'id': this.id
-    };
-    this.detailButton.onClick.subscribe(event => {
-      this.reportStoreService.openFillReport("1685066d-97ee-4b1e-a18a-278dabc5c7a0", this.params, {});
-    });
+  openBranchReportStore() {
+    this.reportStoreService.openFillReport("96c0aa72-2582-4aa4-a93a-afe34acb77c6", {}, {});
   }
+
+  openBranchDetailReportStore(data: any) {
+    const params = {
+      'id': data.OFFICEID
+    };
+    this.reportStoreService.openFillReport("1685066d-97ee-4b1e-a18a-278dabc5c7a0", params, {});
+  }
+
 
 }
