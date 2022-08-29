820fc99de2a187ed004a159d355fc7eced6eeb8f
Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
2022-08-29 11:34:40 +0200

    Updated ontimize-web-ngx version (#89)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   apiEndpoint: 'https://try.imatia.com/ontimizeweb/services/qsallcomponents-jee/services/rest',
   production: true,
   versions: {
-    core: '8.7.0',
+    core: '8.7.1',
     charts: '8.0.1',
     filemanager: '8.5.4',
     map: '8.0.1',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   apiEndpoint: 'https://try.imatia.com/ontimizeweb/services/qsallcomponents-jee/services/rest',
   production: false,
   versions: {
-    core: '8.7.0',
+    core: '8.7.1',
     charts: '8.0.1',
     filemanager: '8.5.4',
     map: '8.0.1',
