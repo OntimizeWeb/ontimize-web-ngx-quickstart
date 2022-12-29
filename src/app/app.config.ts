f8986eda52b54c8a77c4f34e8659d7c68e7dbec7
Daniel Grana <daniel.grana@imatia.com>
2022-12-29 18:08:19 +0100

    Bump released versions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,9 +7,9 @@ export const CONFIG: Config = {
   // The base path of the URL used by app services.
   apiEndpoint: environment.apiEndpoint,
 
-  bundle: {
-    path: 'bundle'
-  },
+  // bundle: {
+  //   path: 'bundle'
+  // },
 
   //  Application identifier. Is the unique package identifier of the app.
   // It is used when storing or managing temporal data related with the app. By default is set as 'ontimize-web-uuid'.
