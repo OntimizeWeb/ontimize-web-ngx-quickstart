d6d300c7b1e2b292e68a2f6f8ee2f5b468e611e4
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2023-03-02 13:11:57 +0100

    Updated versions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   apiEndpoint: 'http://45.84.208.122:9092/qsallcomponents-jee/services/rest',
   production: true,
   versions: {
-    core: '8.8.0',
-    charts: '8.0.1',
-    filemanager: '8.5.5',
+    core: '8.11.0',
+    charts: '8.2.0',
+    filemanager: '8.5.6',
     map: '8.0.1',
-    theming: '8.4.0',
-    report: '8.0.2'
+    theming: '8.5.0',
+    report: '8.1.1'
   }
 };
