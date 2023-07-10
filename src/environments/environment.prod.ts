4a499be1b6371f345bce3ad8110510427397f0cc
gonzalo-martinez-imt <gonzalo.martinez@imatia.com>
2023-07-10 14:00:28 +0200

    Update ontimize-web-ngx-filemanager

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   versions: {
     core: '8.13.0',
     charts: '8.2.0',
-    filemanager: '8.5.6',
+    filemanager: '8.6.1',
     map: '8.0.1',
     theming: '8.5.2',
     report: '8.1.1'
