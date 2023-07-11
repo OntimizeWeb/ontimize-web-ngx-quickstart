65db75a4042b6c1c7ba580a6218e4279ddc48fc2
Gonzalo Martínez Fernández <gonzalo.martinez@imatia.com>
2023-07-11 10:20:08 +0200

    Update to ontimize-web-ngx 8.13.0 (#179)
    * Update to ontimize-web-ngx 8.13.0
    
    * Update environments
    
    * Update package-lock
    
    * Update ontimize-web-ngx-theming
    
    * Update ontimize-web-ngx-filemanager

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   apiEndpoint: 'https://try.imatia.com/ontimizeweb/services/qsallcomponents-jee/services/rest',
   production: true,
   versions: {
-    core: '8.11.0',
+    core: '8.13.0',
     charts: '8.2.0',
-    filemanager: '8.5.6',
+    filemanager: '8.6.1',
     map: '8.0.1',
-    theming: '8.5.0',
+    theming: '8.5.2',
     report: '8.1.1'
   }
 };
