105f81e2c0e3c25dc0534ea0eab5959b19574068
tomfacal <88186642+tomfacal@users.noreply.github.com>
2023-08-25 09:50:22 +0200

    updated to angular 11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from 'ontimize-web-ngx';
 
 export const routes: Routes = [
@@ -16,9 +16,11 @@ export const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full' }
 ];
 
-const opt = {
+const opt: ExtraOptions = {
   enableTracing: false
   // true if you want to print navigation routes
+  ,
+  relativeLinkResolution: 'legacy'
 };
 
 @NgModule({
