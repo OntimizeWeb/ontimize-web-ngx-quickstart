eb56dc41f116e055bc78ceb7aae0030deabea09b
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2024-02-21 13:31:33 +0100

    Updated ontimize dependencies

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   apiEndpoint: 'https://try.imatia.com/ontimizeweb/services/qsallcomponents-jee/services/rest',
   production: true,
   versions: {
-    core: '15.0.0-rc.o',
-    charts: '"15.0.0-beta.1',
-    filemanager: '15.0.0-beta.0',
-    map: '15.0.0-beta.1',
+    core: '15.0.0',
+    charts: '"15.0.0',
+    filemanager: '15.0.0',
+    map: '15.0.0',
     report: '15.0.0-beta.1'
   }
 };
