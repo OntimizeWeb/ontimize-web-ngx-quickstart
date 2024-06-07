f776747e1686b60773a377063aa9945f0114595c
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2024-06-07 12:38:07 +0200

    Updated ontimize-web-ngx version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   apiEndpoint: 'https://appwsc-qsallcomponents.ontimize.com/qsallcomponents-jee/services/rest',
   production: true,
   versions: {
-    core: '8.14.10',
+    core: '8.14.15',
     charts: '8.2.0',
     filemanager: '8.6.1',
     map: '8.0.1',
     theming: '8.5.2',
-    report: '8.4.1'
+    report: '8.4.2'
   }
 };
