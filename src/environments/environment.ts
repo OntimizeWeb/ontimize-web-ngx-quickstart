fc688347eb55d0bcb5137efa31c8c2feb9fbd8c1
nprol <134269709+nprol@users.noreply.github.com>
2026-03-17 12:29:44 +0100

    Update employees view to o-data-view component (#264)
    * chore:  implement data view for employee management
    
    * feat: complete data-view example and update ontimize core and extra-components
    
    * chore: remove extra-components tgz
    
    * fix: filterBuilder target
    
    * Dumping versions and add form-layout-manager sidenav in employees
    
    ---------
    
    Co-authored-by: Daniel <daniel.grana@imatia.com>
    Co-authored-by: Noelia Prol Carballo <noelia.prol@imatia.com>
    Co-authored-by: Patricia Martinez Tilve <patricia.martinez@imatia.com>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,10 +7,11 @@ export const environment = {
   apiEndpoint: 'https://appwsc-qsallcomponents.ontimize.com/qsallcomponents-jee/services/rest',
   production: false,
   versions: {
-    core: '15.7.1',
-    charts: '15.2.1',
+    core: '15.9.0',
+    charts: '15.3.0',
     filemanager: '15.1.1',
     map: '15.0.1',
-    report: '15.1.0'
+    report: '15.1.0',
+    extraComponentes:'15.0.0'
   }
 };
