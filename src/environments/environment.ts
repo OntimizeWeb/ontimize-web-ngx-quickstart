7655664cfdf659c46e6e80575374b54f6ce1f046
Patricia Martinez Tilve <patricia.martinez@imatia.com>
2026-09-15 16:12:32 +0200

    Bumping ontimize-web-ngx version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,11 +7,11 @@ export const environment = {
   apiEndpoint: 'https://appwsc-qsallcomponents.ontimize.com/qsallcomponents-jee/services/rest',
   production: false,
   versions: {
-    core: '18.0.0-next.2',
+    core: '18.0.0-next.20',
     charts: '18.0.0-next.0',
     filemanager: '18.0.0-next.0',
     map: '18.0.0-next.0',
     report: '18.0.0-next.0',
-    extraComponentes: '18.0.0-next.1'
+    extraComponentes: '18.0.0-next.5'
   }
 };
